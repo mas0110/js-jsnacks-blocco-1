f28ad5e40d6a8c15384d92bbce83251d897fff25
mas0110 <masoudmohammed13@gmail.com>
2024-01-11 21:36:44 +0100

    blocco 2 snack 3

--- Blocoo 2 - snack 3/main.js
+++ Blocoo 2 - snack 3/main.js
@@ -1,2 +1,20 @@
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
 // 
+
+
+function sommaDispari(numeriInteri) {
+    let somma = 0;
+  
+    for (let i = 1; i < numeriInteri.length; i += 2) {
+      somma += numeriInteri[i];
+    }
+  
+    return somma;
+  }
+
+const numeriInteri = [ 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,]
+
+  let risultato = sommaDispari(numeriInteri);
+  
+  console.log("La somma degli elementi in posizione dispari è: " + risultato);
+  
