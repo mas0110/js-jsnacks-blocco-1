d779601c331f1b4c0d7ec9048ff11177677745b1
mas0110 <masoudmohammed13@gmail.com>
2024-01-11 20:51:42 +0100

    blocco 2 snack 2

--- Blocco 2 - snack 2/main.js
+++ Blocco 2 - snack 2/main.js
@@ -1,1 +1,70 @@
 // Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
+
+let nome1 = prompt("scegli il primo nome ")
+
+let nome2 = prompt("scegli il secondo nome ")
+// 
+let nome3 = prompt("scegli il terzo nome ")
+
+let cognome1 = prompt("scegli il primo cognome")
+
+let cognome2 = prompt("scegli il secondo cognome")
+// 
+let cognome3 = prompt("scegli il terzo cognome")
+// 
+
+
+
+// 
+const getRandomInt = ( min = 0, max = 1 ) => {
+	return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
+}
+
+let nomerandom = document.querySelector("div.risultatonomi")
+
+const listanomi = [ nome1, nome2, nome3 ],
+
+	totnomi = listanomi.length,
+	totnomirandom = totnomi - 1,
+	resultnomi = getRandomInt( 0, totnomirandom );
+	
+nomerandom.innerHTML = ( listanomi[resultnomi] );
+
+
+let cognomerandom = document.querySelector("div.risultatocognomi")
+
+ const listacognomi = [ cognome1, cognome2, cognome3 ],
+ 
+ 	totcognomi = listacognomi.length,
+ 	totcognomirandom = totcognomi - 1,
+ 	resultcognome = getRandomInt( 0, totcognomirandom );
+ 	
+ cognomerandom.innerHTML = ( listacognomi[resultcognome] );
+
+
+
+
+document.getElementById("again").addEventListener("click", function(){
+
+    let nomerandom = document.querySelector("div.risultatonomi")
+
+    const listanomi = [ nome1, nome2, nome3 ],
+    
+    	totnomi = listanomi.length,
+    	totnomirandom = totnomi - 1,
+    	resultnomi = getRandomInt( 0, totnomirandom );
+    	
+    nomerandom.innerHTML = ( listanomi[resultnomi] );
+    
+    
+    let cognomerandom = document.querySelector("div.risultatocognomi")
+    
+    const listacognomi = [ cognome1, cognome2, cognome3 ],
+    
+    	totcognomi = listacognomi.length,
+    	totcognomirandom = totcognomi - 1,
+    	resultcognome = getRandomInt( 0, totcognomirandom );
+    	
+    cognomerandom.innerHTML = ( listacognomi[resultcognome] );
+
+})
