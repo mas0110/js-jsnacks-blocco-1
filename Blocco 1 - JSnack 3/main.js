962f0b45ad7f4118421e4ea7a212c46a9eede79b
mas0110 <masoudmohammed13@gmail.com>
2024-01-11 16:21:29 +0100

    esercizio blocco 1 snack 3

--- Blocco 1 - JSnack 3/main.js
+++ Blocco 1 - JSnack 3/main.js
@@ -1,3 +1,26 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 // 
+
+let numero1 = +prompt("inserisci il primo numero")
+
+let numero2 = +prompt("inserisci il secondo numero")
+
+let numero3 = +prompt("inserisci il terzo numero")
+
+let numero4 = +prompt("inserisci il quarto numero")
+
+let numero5 = +prompt("inserisci il quinto numero")
+
+let numero6 = +prompt("inserisci il sesto numero")
+
+let numero7 = +prompt("inserisci il settimo numero")
+
+let numero8 = +prompt("inserisci l'ottavo numero")
+
+let numero9 = +prompt("inserisci il nono numero")
+
+let numero10 =+ prompt("inserisci il decimo numero")
+
+
+alert(numero1 + numero2 + numero3 +numero4 + numero5 + numero6 +numero7 + numero8 + numero9 + numero10)
